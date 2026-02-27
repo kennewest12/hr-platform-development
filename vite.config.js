df28445846ab5fa6d43cda5ab4f41de2647102c5
Kenneth Chibuzor Okonkwo <kennethokonkwo73@gmail.com>
2026-02-27 07:51:52 +0100

    fix: optimize build chunks and increase size limit for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // This splits vendor libraries (like React/Lucide) into separate files
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increases warning threshold to 1MB
+  },
 });
